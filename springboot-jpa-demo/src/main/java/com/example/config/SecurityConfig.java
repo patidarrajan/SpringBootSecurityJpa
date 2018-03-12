4774c7ce8c5add7ae065576d71c21ca94e5f4a51
rajan.patel <rajan.patel@einfochips.com>
2018-03-12 11:08:30 +0530

    Basic Auth

--- springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
+++ springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.service.UserDetailsServiceImpl;
 
@@ -24,31 +21,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationEntryPoint authenticationEntryPoint;
 
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+	// @Override
+	// @Bean
+	// public AuthenticationManager authenticationManagerBean() throws Exception
+	// {
+	// return super.authenticationManagerBean();
+	// }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()/*.antMatchers("/*").hasAnyRole("ADMIN", "USER")*/
+		http.csrf().disable().authorizeRequests().antMatchers("/*").hasAnyRole("ADMIN", "USER")
 				.antMatchers(HttpMethod.DELETE, "/user/*").hasAnyRole("ADMIN").antMatchers(HttpMethod.POST, "/user/*")
 				.hasAnyRole("ADMIN").antMatchers(HttpMethod.PUT, "/user/*").hasAnyRole("ADMIN")
-				.antMatchers(HttpMethod.GET, "/user/*").hasAnyRole("ADMIN", "USER").antMatchers("/oauth/token")
-				.permitAll()/*.and().httpBasic()
-				.realmName("Topic security application Realm").authenticationEntryPoint(authenticationEntryPoint)*/;
+				.antMatchers(HttpMethod.GET, "/user/*").hasAnyRole("ADMIN", "USER").and().httpBasic()
+				.realmName("Topic security application Realm").authenticationEntryPoint(authenticationEntryPoint);
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	// @Bean
+	// public PasswordEncoder passwordEncoder() {
+	// return new BCryptPasswordEncoder();
+	// }
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		// BCryptPasswordEncoder bCryptPasswordEncoder = new
 		// BCryptPasswordEncoder();
-		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
