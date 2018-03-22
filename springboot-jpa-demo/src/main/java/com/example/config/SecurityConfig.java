555a5f42236dc80985b58308688dd23dbdedc0a3
rajan.patel <rajan.patel@einfochips.com>
2018-03-22 18:29:53 +0530

    Removed additional files and comments

--- springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
+++ springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
@@ -16,34 +16,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
-
-	@Autowired
-	private AuthenticationEntryPoint authenticationEntryPoint;
-
-	// @Override
-	// @Bean
-	// public AuthenticationManager authenticationManagerBean() throws Exception
-	// {
-	// return super.authenticationManagerBean();
-	// }
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/").hasAnyRole("ADMIN")
 				.antMatchers("/paper/**")
-				.hasAnyRole("ADMIN", "USER").antMatchers("/user/**").hasAnyRole("ADMIN").and().httpBasic()
-				.realmName("Topic security application Realm").authenticationEntryPoint(authenticationEntryPoint);
+				.hasAnyRole("ADMIN", "USER").antMatchers("/user/**").hasAnyRole("ADMIN").and().httpBasic();
 	}
-
-	// @Bean
-	// public PasswordEncoder passwordEncoder() {
-	// return new BCryptPasswordEncoder();
-	// }
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		// BCryptPasswordEncoder bCryptPasswordEncoder = new
-		// BCryptPasswordEncoder();
 		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
