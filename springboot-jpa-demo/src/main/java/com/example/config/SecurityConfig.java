7666a728c4f8d5c14fc1d27affcdb6befc805d75
rajan.patel <rajan.patel@einfochips.com>
2018-03-23 10:13:48 +0530

    Oauth implementation added

--- springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
+++ springboot-jpa-demo/src/main/java/com/example/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,15 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers("/").hasAnyRole("ADMIN")
 				.antMatchers("/paper/**")
-				.hasAnyRole("ADMIN", "USER").antMatchers("/user/**").hasAnyRole("ADMIN").and().httpBasic();
+				.hasAnyRole("ADMIN", "USER").antMatchers("/user/**").hasAnyRole("ADMIN").antMatchers("/oauth/token")
+				.permitAll().and().formLogin().permitAll();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// auth.inMemoryAuthentication().withUser("john").password("123").roles("USER");
 		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
--- springboot-jpa-demo/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-  INSERT INTO `users` (`username`, `password`, `full_name`, `role`, `country`, `enabled`) VALUES
-	('sean', '$2a$10$VtOI/NbNfv.K7rUBiEMSHOnb6sXyf2xvziCYJ7qqZXLLBcpc6Bmvy', 'Sean Murphy', 'ROLE_ADMIN', 'US', 1),
-	('martin', 'b36dWqOE0ihHFIZymMO6meh9GaJbjIa7tOvQCOnPYa5Vo7wJJKsEi', 'Martin Bingel', 'ROLE_USER', 'India', 1); 
